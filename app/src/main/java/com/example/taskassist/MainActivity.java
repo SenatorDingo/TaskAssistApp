b314b31e0b95fa77e96d6175ebfc7f6f44ddc52d
SenatorDingo <abhayabhilash@duck.com>
2023-06-16 11:30:06 -0400

    Updated Adapter

--- app/src/main/java/com/example/taskassist/MainActivity.java
+++ app/src/main/java/com/example/taskassist/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TaskList taskList;
+    private List<Task> listTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Task task = new Task("Do something", "11/28", 1);
+
+
     }
 }
--- app/src/main/java/com/example/taskassist/TaskList.java
+++ app/src/main/java/com/example/taskassist/TaskList.java
@@ -1,13 +1,17 @@
 package com.example.taskassist;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
+import java.util.PriorityQueue;
 
 
-public class TaskList extends ArrayAdapter<Task> {
+public class TaskList extends ArrayAdapter<Task>{
 
     private final Activity context;
     private List<Task> tasks;
@@ -16,6 +20,19 @@ public class TaskList extends ArrayAdapter<Task> {
         super(context, R.layout.layout_task_list, tasks);
         this.context = context;
         this.tasks = tasks;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.layout_task_list, null, true);
+
+        TextView textViewTitle = (TextView) listViewItem.findViewById(R.id.textViewTitle);
+        TextView textViewDeadline = (TextView) listViewItem.findViewById(R.id.textViewDeadline);
+
+        Task task = tasks.get(position);
+        textViewTitle.setText(task.getTitle());
+        textViewDeadline.setText(task.getDeadline());
+        return listViewItem;
     }
 
 }
